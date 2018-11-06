32ac120fd002b9a141364e95f1430b28eb470c37
Zac Seewald <zs@Zacs-MacBook-Pro.local>
2018-11-05 18:37:38 -0600

    reset all the styles and removed bootstrap. got the counter to work with the press play button

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-    $(document).on('click', function(){
-        newQuestion();
-    });
+
+
+
+    
+
         // ? for the user
         //   aswers with buttons
      
@@ -9,7 +11,7 @@ $(document).ready(function () {
         //  correct -> increase correctAcc, move to next ?
         //  incorrect -> increase incorrectAcc, move to next ?
         //  at the end - show results
-     
+
         var timer;
         var countDown = 20;
      
@@ -23,12 +25,68 @@ $(document).ready(function () {
                 correctAnswer: "yellow",
             }
         ];
-     function newQuestion() {
-        timer = setInterval(countDown, 1000);
-        $("#questAndAnswer").html(questions.question);
-        for( var i=0; i < questions.answers.length; i++ ){
-            $("#questAndAnswer").append('<button>' + questions.answers[i] + '</button>');
+
+    // sets the interval
+    function timerInterval() {
+        timer = setInterval(counter, 1000);
+    };
+    //counts down the timer
+    function counter() {
+        countDown--;
+        $("#display-timer").text("Time Left = " + countDown + " sec");
+        if (countDown <= 0) {
+            clearInterval(timer);
+
         }
-     }
+    };
+        // sets the timmer on the page
+        $("#playGame").on('click', function(){
+    
+            $("#display-timer").empty();
+            $("#display-timer").text("Time Remaining: " + countDown + " seconds");
+            timerInterval();
+            counter();
+
+            
+        });
+
+   
+     
+     
+ //Mike's code for timerbelow
+     function timer() {
+        intervalID = setInterval(countdown, 1000)
+     };
+     
+     function countdown() {
+        time--;
+        $("#time-display").text("Time left: " + time);
+        if (time === 0) {
+            unAnswered++;
+            console.log(unAnswered);
+            stop();
+            $("#time-display").text("Time's up!");
+            displayQuestion(quizQuestions2[questionIndex++]);
+            reset();
+        }
+     };
+     
+     function stop() {
+        clearInterval(intervalID)
+     };
+     
+     function reset() {
+        clearInterval(intervalID);
+        time = 25;
+        timer();
+     };
+
+    //  function newQuestion() {
+    //     timer = setInterval(countDown, 1000);
+    //     $("#questAndAnswer").html(questions.question);
+    //     for( var i=0; i < questions.answers.length; i++ ){
+    //         $("#questAndAnswer").append('<button>' + questions.answers[i] + '</button>');
+    //     }
+    //  }
      
      });
